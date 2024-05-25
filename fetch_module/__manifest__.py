ba873ce1db2ea1deccb89977a452acfc45f66b29
muhrizky <muhrizqi@student.ce.undip.ac.id>
2024-05-25 09:07:17 +0100

    [IMP] fetch_module: add dependent auth_module

--- fetch_module/__manifest__.py
+++ fetch_module/__manifest__.py
@@ -1,19 +1,19 @@
 {
-  'name': 'Fetch Module',
-  'version': '1.0.0',
-  'category': 'Tools',
-  'author': 'Muhammad Rizqi',
-  'license': 'AGPL-3',
-  'summary': 'Fetch data, add USD price, and enforce role-based access control',
-  'description': '',
-  'depends': [
-    'base',
-  ],
-  'data': [
-    'security/ir.model.access.csv',
-    "views/views.xml",
-    "views/fish_price.xml",
-  ],
-  'installable': True,
-  'auto_install': False,
+    'name': 'Fetch Module',
+    'version': '1.0.0',
+    'category': 'Tools',
+    'author': 'Muhammad Rizqi',
+    'license': 'AGPL-3',
+    'summary': 'Fetch data, add USD price, and enforce role-based access control',
+    'description': '',
+    'depends': [
+        'base', 'auth_module'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        "views/views.xml",
+        "views/fish_price.xml",
+    ],
+    'installable': True,
+    'auto_install': False,
 }
